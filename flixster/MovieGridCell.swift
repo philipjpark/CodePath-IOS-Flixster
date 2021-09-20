c3a8856f2c902f7082ba4ef4acb145cdeccee8de
philipjpark <philipparknyc@gmail.com>
2021-09-20 09:00:15 -0400

    Completed Flixster

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Philip Park on 9/20/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
